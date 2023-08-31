7bd3a26ba7ec2ac2510199a700ef04cf246ad0bc
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-08-31 21:01:06 +0300

    Fixed wrong type-annotation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,7 +5,7 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List, redaction: str,
+def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """
         This func replaces a matching str in another string with a specific str
