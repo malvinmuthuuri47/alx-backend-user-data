cf170160c2127c02378593b4db2bc09fe287f15a
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-08-31 20:57:16 +0300

    Updated func to be less than 5 lines

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,13 +9,6 @@ def filter_datum(fields: List, redaction: str,
                  message: str, separator: str) -> str:
     """
         This func replaces a matching str in another string with a specific str
-
-        Args:
-            fields: A list of strings representing all fields to obfuscate
-            redaction: str representing by what the field will be obfuscated
-            message: str representing the log line
-            separator: sre representation of a char separating fields of the
-                       log line
     """
     return re.sub(rf"({'|'.join(fields)})=[^{separator}]*",
                   rf"\1={redaction}", message)
