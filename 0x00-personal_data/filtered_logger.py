9ad739294f0ddabb60d4eb19308f8ea452b6b70a
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-09-01 09:34:59 +0300

    Added main guard to the code and fixed type-annotation for the constructor

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -38,3 +38,6 @@ def filter_datum(fields: List[str], redaction: str,
     """
     return re.sub(rf"({'|'.join(fields)})=[^{separator}]*",
                   rf"\1={redaction}", message)
+
+if __name__ == '__main__':
+    main()
