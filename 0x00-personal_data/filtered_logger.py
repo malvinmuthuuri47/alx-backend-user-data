3159f5f6e68cb5d82b97e143474daf104f3011d4
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-09-02 21:33:39 +0300

    Changed how the message is being access by accessing it from the superclass

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -24,14 +24,13 @@ class RedactingFormatter(logging.Formatter):
         """The constructor function that accepts fields and calls the
         super class constructor to initialize the child class"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields if fields else PII_FIELDS
+        self.fields = fields if fields
 
     def format(self, record: logging.LogRecord) -> str:
         """Formats the specified log record as text.
         Filters values in incoming log record using filter_datum"""
-        record.msg = filter_datum(self.fields, self.REDACTION,
-                                  record.getMessage(), self.SEPARATOR)
-        return super(RedactingFormatter, self).format(record)
+        msg = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
 
 
 def filter_datum(fields: List[str], redaction: str,
