047eb802db69d1535e7995a11d8d7bebfd1150ac
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-09-04 21:58:51 +0300

    Error handler:Forbidden

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -29,7 +29,16 @@ def stats() -> str:
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
     """ GET /api/v1/unauthorized
-    Return :
+    Return:
         - A 401 error raised using abort
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+        - A 403 error raised using abort
+    """
+    abort(403)
