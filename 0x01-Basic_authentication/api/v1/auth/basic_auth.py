ae62a281aea5d532432908fdcb407117d89e02d4
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-09-05 14:35:58 +0300

    Basic - User object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,8 @@
 from api.v1.auth.auth import Auth
 import binascii
 import base64
+from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -94,3 +96,30 @@ class BasicAuth(Auth):
         else:
             return (None, None)
         return
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str
+            ) -> TypeVar('User'):
+        """This function searches the database file for instances that
+        contain the email and password provided.
+
+        Requirements:
+            - If user_email is None or not a string, return None
+            - If user_pwd is None or not a string, return None
+            - Search for instances of the User class and if none are
+              found, return None.
+            - Return None if the password is not valid for the User
+              instance found
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        try:
+            users = User.search({"email": user_email})
+        except Exception:
+            return None
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None
