453cdd6cf2e2af3c34a7f33385a33624adc4d075
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-09-08 13:38:45 +0300

    New view for Session Authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -12,11 +12,11 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-basic_auth = None
+session_auth = None
 AUTH_TYPE = getenv('AUTH_TYPE')
-if AUTH_TYPE == "basic_auth":
-    from api.v1.auth.basic_auth import BasicAuth
-    auth = BasicAuth()
+if AUTH_TYPE == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 
 
 @app.before_request
---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+""" Module for Session views """
+
+from flask import request, jsonify, abort, make_response
+from api.v1.views import app_views
+from models.user import User
+import os
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login():
+    """ POST /api/v1/auth_session/login
+    Return:
+        - The User instance based on the email
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if email is None or email == '':
+        return jsonify({"error": "email missing"}), 400
+    elif password is None or password == '':
+        return jsonify({"error": "password missing"}), 400
+
+    # Retrieve the User instance based on the Email
+    users = User.search({'email': email})
+    if not users:
+        return jsonify({"error": "no user found for this email"}), 404
+    else:
+        for user in users:
+            if user.is_valid_password(password):
+                from api.v1.app import auth
+                session_id = auth.create_session(user.id)
+                dict_repr_user = jsonify(user.to_json())
+                session_name = os.getenv('SESSION_NAME')
+                dict_repr_user.set_cookie(session_name, session_id)
+                return dict_repr_user
+        return jsonify({"error": "wrong password"}), 401
