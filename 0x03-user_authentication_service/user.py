1f18bd0d729bff1a7aed2d4527e9350472928e66
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-09-12 11:12:45 +0300

    User model

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""User model"""
+
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+engine = create_engine('sqlite:///:memory:', echo=True)
+
+Base = declarative_base()
+
+
+class User(Base):
+    """This class defines the structure of a user model for a Database
+    using SQLAlchemy with various attributes that can be stored in a
+    database"""
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
