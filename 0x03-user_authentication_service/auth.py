f20414f7e548685aac302c041454be583841bc38
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-09-12 17:47:17 +0300

    Updated solution for task four

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,12 +3,13 @@
 
 import bcrypt
 
+
 def _hash_password(password: str) -> bytes:
     """This function handles the hashing part of the code by first
     converting the password to an array of bytes, then generating
     salt ant and hashing the bytes"""
-    bytes = password.encode('utf-8')
+    bytes_passwd = password.encode('utf-8')
     salt = bcrypt.gensalt()
-    hash = bcrypt.hashpw(bytes, salt)
+    hashed_passwd = bcrypt.hashpw(bytes_passwd, salt)
 
-    return hash
+    return hashed_passwd
