ba6a54cb17d0afcac7274dfbccc9e64cac2fa497
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-09-14 18:23:51 +0300

    Updated solution for task 14

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -48,29 +48,5 @@ def login():
         return jsonify(response_data), 200
 
 
-@app.route('/sessions', methods=['DELETE'])
-def logout() -> str:
-    """Delete session"""
-    session_id = request.cookies.get("session_id")
-    user = AUTH.get_user_from_session_id(session_id)
-    if user:
-        AUTH.destroy_session(user.id)
-        return redirect('/')
-    else:
-        abort(403)
-
-
-@app.route('/profile', methods=['GET'])
-def profile() -> str:
-    """Profile function"""
-    session_id = request.cookies.get("session_id")
-    user = AUTH.get_user_from_session_id(session_id)
-    if user:
-        user_email = user.email
-        return jsonify({"email": user_email}), 200
-    else:
-        abort(403)
-
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000", debug=True)
