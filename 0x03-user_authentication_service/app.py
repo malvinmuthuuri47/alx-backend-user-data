f48892553774817f85e387d9bca0e4ad8cab6be8
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-09-15 14:12:14 +0300

    Updated solution to task 19

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -91,7 +91,7 @@ def update_password() -> str:
     reset_token = request.form.get("reset_token")
     new_password = request.form.get("new_password")
     try:
-        user = AUTH.update_password(reset_token, new_password)
+        AUTH.update_password(reset_token, new_password)
         return jsonify({"email": {email}, "message": "Password updated"})
     except Exception:
         abort(403)
